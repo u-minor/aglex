bd172b84c7238c69c8f91cfeae15fb67b24fc5c5
Minoru Nakata <minoru@sprocket.bz>
2015-11-12 21:46:41 +0900

    create basic libs

---
+++ bin/aglex.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var debug = require('debug')('aglex.bin');
+var yaml = require('js-yaml');
+var yargs = require('yargs')
+  .usage('Usage: $0 <command> ... [options]')
+  .command('apigateway', 'command for API Gateway', function(yargs) {
+    argv = yargs
+      .usage('Usage: $0 apigateway <subcommand> [options]')
+      .command('update', 'Create/update API')
+      .demand(2, 'must provide a valid subcommand')
+      .option('config', {alias: 'c', demand: true, nargs: 1, type: 'string', describe: 'YAML config file'})
+      .help('help').alias('help', 'h')
+      .argv;
+  })
+  .command('lambda', 'Commands for Lambda', function(yargs) {
+    argv = yargs
+      .usage('Usage: $0 lambda <subcommand> [options]')
+      .command('update', 'Create/update Lambda function', function(yargs) {
+        argv = yargs
+          .usage('Usage: $0 lambda update [options]')
+          .option('config', {alias: 'c', demand: true, nargs: 1, type: 'string', describe: 'YAML config file'})
+          .option('zip', {alias: 'z', demand: true, nargs: 1, type: 'string', describe: 'Lambda zip file'})
+          .help('help').alias('help', 'h')
+          .argv;
+      })
+      .command('add-permission', 'Add execute-api permission to the function', function(yargs) {
+        argv = yargs
+          .usage('Usage: $0 lambda add-permission [options]')
+          .option('config', {alias: 'c', demand: true, nargs: 1, type: 'string', describe: 'YAML config file'})
+          .help('help').alias('help', 'h')
+          .argv;
+      })
+      .demand(2, 'must provide a valid subcommand')
+      .option('config', {alias: 'c', demand: true, nargs: 1, type: 'string', describe: 'YAML config file'})
+      .help('help').alias('help', 'h')
+      .argv;
+  })
+  .demand(1, 'must provide a valid command')
+  .option('config', {alias: 'c', demand: true, nargs: 1, type: 'string', describe: 'YAML config file'})
+  .help('help').alias('help', 'h');
+var argv = yargs.argv;
+
+var config = (function(file) {
+  var data, cwd = process.cwd();
+  process.chdir(path.dirname(argv.config));
+  data = yaml.safeLoad(fs.readFileSync(path.basename(argv.config), 'utf8'));
+  process.chdir(cwd);
+  return data;
+})(argv.config);
+
+var aglex = require('../lib/aglex')(config, 'info');
+
+switch (argv._[0]) {
+  case 'apigateway':
+    switch (argv._[1]) {
+      case 'update':
+        console.log('Updating API ...');
+        aglex.updateApi().then(function() {
+          console.log('Completed.');
+        }, function(err) {
+          debug(err);
+          console.log('Failed.');
+        });
+        break;
+      default:
+        yargs.showHelp();
+    }
+    break;
+  case 'lambda':
+    switch (argv._[1]) {
+      case 'update':
+        console.log('Updating lambda function ...');
+        aglex.updateLambda(argv.zip).then(function() {
+          console.log('Completed.');
+        }, function(err) {
+          debug(err);
+          console.log('Failed.');
+        });
+        break;
+      case 'add-permission':
+        console.log('Adding execute-api permission to the lambda function ...');
+        aglex.addLambdaPermission().then(function() {
+          console.log('Completed.');
+        }, function(err) {
+          debug(err);
+          console.log('Failed.');
+        });
+        break;
+      default:
+        yargs.showHelp();
+    }
+    break;
+  default:
+    yargs.showHelp();
+}
